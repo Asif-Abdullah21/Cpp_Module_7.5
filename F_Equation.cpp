f1d10047f1d58508608a7a99489e59299ef78554
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-06-01 02:47:54 +0600

    Sheet #5 (Functions), problem: (F) Equation

--- F_Equation.cpp
+++ F_Equation.cpp
@@ -1,35 +1,21 @@
 #include <bits/stdc++.h>
-#define endl "\n"
-#define nn (cout << "NO\n")
-#define yy (cout << "YES\n")
-#define ss " "
-#define sort(a) sort(a,a+n)
-#define rev(a) reverse(a,a+n)
-#define ll long long
-#define ull unsigned long long
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) ((a / gcd(a, b)) * b)
-#define pii pair<int, int>
-#define pll pair<long long, long long>
-#define sz(v) v.size()
-#define mm(a, x) memset(a, x, sizeof(a))
-#define FIO                       \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
 using namespace std;
 
-void solve(ll x,ll n)
+void solve(int x,int n)
 {
-    ll ans =1;
+    long long ans = 0;
     for (int i = 2; i <= n; i+=2)
     {
-       // ans += pow(x,i);
-       for(int j=1;j<=i;j+=2)
-       {
-           ans = ans*x;
-       }
+        long long temp=1;
+        for(int j=1;j<=i;j++)
+        {
+            temp *= x;
+        }
+
+        ans += temp;
     }
+    
+    
 
     cout << ans << endl;
     
@@ -37,10 +23,39 @@ void solve(ll x,ll n)
 
 int main()
 {
-    FIO;
-    ll x,n;
-    cin >>x>> n;
+    int x,n;
+    cin >> x >> n;
 
-    solve(x,n); 
+    solve(x,n);
+     
     return 0;
 }
+
+
+//-----------------------------------------------------------------
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void solve(int x,int n)
+// {
+//     long long ans = 0,val=1;
+//     for (int i = 2; i <= n; i+=2)
+//     {
+//         ans += (long long)(pow(x,i)+.5);
+//     }
+
+//     cout << ans << endl;
+    
+// }
+
+// int main()
+// {
+//     int x,n;
+//     cin >> x >> n;
+
+//     solve(x,n);
+     
+//     return 0;
+// }

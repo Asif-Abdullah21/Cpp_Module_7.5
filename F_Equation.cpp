7959e23e2f4d8980fa14b6d9daf996832b1571cf
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-06-04 19:56:37 +0600

    contest: Sheet #5 (Functions), problem: (F) Equation

--- F_Equation.cpp
+++ F_Equation.cpp
@@ -1,34 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve(int x,int n)
+void solve(long x,long n)
 {
-    long long ans = 0;
+    double ans = 0;  //Rahat_Khan_Pathan vai
     for (int i = 2; i <= n; i+=2)
     {
-        long long temp=1;
-        for(int j=1;j<=i;j++)
-        {
-            temp *= x;
-        }
-
-        ans += temp;
+        ans += pow(x*1.0,i*1.0);
     }
-    
 
-    cout << ans << endl;
+
+    cout << (long long)ans << endl;
     
 }
 
 int main()
 {
-    int x,n;
+    long long x,n;
     cin >> x >> n;
 
     solve(x,n);
      
     return 0;
 }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void solve(int x,int n)
+// {
+//     long long ans = 0;
+//     for (int i = 2; i <= n; i+=2)
+//     {
+//         long long temp=1;
+//         for(int j=1;j<=i;j++)
+//         {
+//             temp *= x;
+//         }
+
+//         ans += temp;
+//     }
+    
+
+//     cout << ans << endl;
+    
+// }
+
+// int main()
+// {
+//     int x,n;
+//     cin >> x >> n;
+
+//     solve(x,n);
+     
+//     return 0;
+// }
 
 
 //-----------------------------------------------------------------
